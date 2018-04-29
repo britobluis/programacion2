6812a37de0bcbd24cfb8a6a4263f418cd320ac0b
sebastorm <sebastianechazu@gmail.com>
2018-04-29 05:03:46 -0300

    saque  loc comentarios y un metodo que no estab en uso

--- TP_Arreglos_Ej3/src/tp_arreglos_ej3/TP_Arreglos_Ej3.java
+++ TP_Arreglos_Ej3/src/tp_arreglos_ej3/TP_Arreglos_Ej3.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp_arreglos_ej3;
 
-import javax.swing.JOptionPane;
 
-/**
- *
- * @author Luis Brito
- */
 public class TP_Arreglos_Ej3 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         int[] primeNumbers = new int[10];
         int i = 0;
