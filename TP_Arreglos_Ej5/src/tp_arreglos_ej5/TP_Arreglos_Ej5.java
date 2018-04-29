1b643169af5da6089ec9e1bbc9c64e4bfd254b45
sebastorm <sebastianechazu@gmail.com>
2018-04-29 05:06:12 -0300

    saque  loc comentarios

--- TP_Arreglos_Ej5/src/tp_arreglos_ej5/TP_Arreglos_Ej5.java
+++ TP_Arreglos_Ej5/src/tp_arreglos_ej5/TP_Arreglos_Ej5.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp_arreglos_ej5;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Luis Brito
- */
+
 public class TP_Arreglos_Ej5 {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         int[] arregloA = new int[10];
         int mayorValor = 0;
